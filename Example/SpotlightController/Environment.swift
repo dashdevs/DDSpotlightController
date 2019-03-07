99df11baa97a9b42aba2dd12af5a097ec7cdc7ac
Kirill Ushkov <kirill.u@itomy.ch>
2019-03-07 21:27:31 +0200

    DDOS-52 added structure for storing dependencies

---
+++ Example/SpotlightController/Environment.swift
@@ -0,0 +1,14 @@
+//
+//  Environment.swift
+//  SpotlightController_Example
+//
+//  Copyright (c) 2019 dashdevs.com. All rights reserved.
+//
+
+import Foundation
+
+struct Environment {
+    var models: [AppModel] = .mocks(15)
+}
+
+var Current = Environment()
